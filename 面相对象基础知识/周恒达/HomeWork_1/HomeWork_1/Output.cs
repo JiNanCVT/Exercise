3d8d150514eb85e97d292defbdbaaee0adc451e5
zhouhengda <1443753547@qq.com>
2012-11-22 19:25:22 +0800

    hengda
    周恒达

---
+++ 面相对象基础知识/周恒达/HomeWork_1/HomeWork_1/Output.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_1
+{
+    class Output
+    {
+        public Output(int  choseInt, IOrderedEnumerable<Student> list1, IOrderedEnumerable<Student> list2)
+        {
+            
+                if (choseInt == 1)
+                {
+                    foreach (Student s in list1)
+                    {
+                        Console.WriteLine(s.name + s.score);
+
+                    }
+                    Console.Read();
+                }
+                else
+                {
+                    if (choseInt == 2)
+                    {
+                        foreach (Student s in list2)
+                        {
+                            Console.WriteLine(s.name + s.score);
+
+                        }
+                        Console.Read();
+                    }
+                    else
+                    {
+                        Console.WriteLine("请输入1或者2");
+                        Console.Read();
+                    }
+                }
+            } 
+        }
+    }
+
