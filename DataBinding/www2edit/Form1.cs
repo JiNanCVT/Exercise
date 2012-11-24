4b4e8b28d0fc4c004c52df7789bcbb78d7c8175f
YuanHao_Wang <wang_yuanhao@live.com>
2012-11-24 09:28:18 +0800

    王伟文edit

---
+++ DataBinding/www2edit/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForm
+{
+     
+    public partial class Form1 : Form
+    {
+        List<string> cities = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+        public void listView2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
