073b5f4d496301cebc7557befbe20f546ccc6328
wdh <1366678737@qq.com>
2012-11-24 11:21:42 +0800

    xwz

---
+++ OOPBase/wdh/Sort_Program/Sort_Program/Sort_Score.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort_Program
+{
+    class Sort_Score
+    {
+        public static int SortStudent(Student s1, Student s2)
+        {
+            if (s1.score.CompareTo(s2.score) != 0)
+                return s1.score.CompareTo(s2.score);
+            else
+                return s1.name.CompareTo(s2.name);
+        }
+    }
+}
---
+++ OOPBase/wdh/Sort_Program/Sort_Program/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort_Program
+{
+    public class Student
+    {
+        public int score;
+        public int Score
+        {
+            get { return score; }
+            set { score = value; }
+        }
+
+        public string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public Student(int _score, string _name)
+        {
+            score = _score;
+            name = _name;
+        }     
+    }
+}
