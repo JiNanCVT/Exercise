d89f6ab465d8cbdb10da1efc59b7d81c9d697ab5
YuanHao_Wang <wang_yuanhao@live.com>
2012-11-25 22:36:23 +0800

    周恒达罗蔚王伟文
    学生列表

---
+++ OOPBase/www_team1/zhouluow/Homework_2/Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+
+namespace Homework_2
+{
+    class Student
+    {
+        public String name;
+
+        public String Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public float  score;
+
+        public float  Score
+        {
+            get { return score; }
+            set { 
+                if (value >=0&&value<=100)
+                score = value;
+            else
+                    MessageBox.Show("请输入0~100的分数！");
+            }
+        }
+        public Student()
+        {
+            this.name = name;
+            this.score = score;
+        }
+    }
+}
