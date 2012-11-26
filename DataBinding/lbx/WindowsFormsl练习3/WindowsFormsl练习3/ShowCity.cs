38b3edc869168e708a6e753dafb0ba25bf57fd3a
lbx <1476659627@qq.com>
2012-11-26 16:57:25 +0800

    lbx

---
+++ DataBinding/lbx/WindowsFormsl练习3/WindowsFormsl练习3/ShowCity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsl练习3
+{
+    class ShowCity
+    {
+        public static string[] p1 = {"广州","珠海" };
+        public static string[] p2 = {"南宁","梧州" };
+    }
+}
