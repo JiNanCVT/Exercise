16d0854e6e3a62e31485e1bf1e39cb2f3134e9c9
zhouhengda <1443753547@qq.com>
2012-12-11 00:05:24 +0800

    dbTest
    书法家的名字可以编辑，删除功能有bug，要点一下delete按钮，再点下textBox，再点下delete按钮才能删除，应该是思路错了。。

---
+++ da/CalligraphySample_2/CalligraphySample/Entities/EntityBase.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalligraphySample.Entities
+{
+    public class EntityBase:INotifyPropertyChanged
+    {
+        //public delegate INotifyPropertyChanged del(object sender, PropertyChangedEventArgs e);
+        public enum Statuses
+        {
+            Updated,
+            New,
+            Edit,
+            Delete
+        }
+
+        private Statuses _status;
+
+        public Statuses Status
+        {
+            get { return _status; }
+            set 
+            { 
+                _status = value; 
+            }
+        }
+        //public void DeletePropertyChanged(string propertyName)
+        //{
+        //    del(this, new PropertyChangedEventArgs(propertyName));
+        //}
+
+         
+
+        public void SetPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public EntityBase()
+        {
+            
+            PropertyChanged += EntityBase_PropertyChanged;
+        }
+
+        void EntityBase_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+                _status = Statuses.Edit;
+        }
+    }
+}
