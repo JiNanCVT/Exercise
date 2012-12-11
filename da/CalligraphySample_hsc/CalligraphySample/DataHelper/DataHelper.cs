25ded284f5ff0b6ebf9e18ea6b729d4daa151579
周恒达 <1443753547@qq.com>
2012-12-11 23:13:07 +0800

    test

--- da/CalligraphySample_hsc/CalligraphySample/DataHelper/DataHelper.cs
+++ da/CalligraphySample_hsc/CalligraphySample/DataHelper/DataHelper.cs
@@ -10,7 +10,7 @@ namespace CalligraphySample.DataHelper
     {
         public static SqlConnection CreateConnection()
         {
-            return new SqlConnection(@"Data Source=HSC_win8;Initial Catalog=CalligraphyTeaching;Persist Security Info=True;User ID=sa;Password=0663");
+            return new SqlConnection(@"Data Source=da\sqlexpress;Initial Catalog=Calligrapher;Integrated Security=True");
         }
     }
 }
